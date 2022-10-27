c7f8652687167066ba12e14a2d0cbecdfa2e87e5
harry <zh_harry@163.com>
2022-10-28 01:04:28 +0800

    init

---
+++ src/main/java/com/sparrow/spring/starter/DatasourceAutoConfiguration.java
@@ -0,0 +1,33 @@
+package com.sparrow.spring.starter;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.sparrow.datasource.DataSourceFactoryImpl;
+import javax.inject.Inject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatasourceAutoConfiguration {
+
+    @Inject
+    private SparrowConfig sparrowConfig;
+
+    @Bean
+    public DruidDataSource sparrow_default() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUsername(this.sparrowConfig.getUsername());
+        druidDataSource.setPassword(this.sparrowConfig.getPassword());
+        druidDataSource.setUrl(this.sparrowConfig.getUrl());
+        druidDataSource.setDriverClassName(this.sparrowConfig.getDriverClassName());
+        druidDataSource.setInitialSize(8);
+        druidDataSource.setMaxActive(8);
+        druidDataSource.setBreakAfterAcquireFailure(true);
+        return druidDataSource;
+    }
+
+    @Bean
+    public DataSourceFactoryImpl dataSourceFactory() {
+        return new DataSourceFactoryImpl("sparrow_default");
+    }
+}
---
+++ src/main/java/com/sparrow/spring/starter/DefaultController.java
@@ -0,0 +1,21 @@
+package com.sparrow.spring.starter;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DefaultController {
+    /**
+     * SpringMVC映射的 / 与 /* 与 /** 的区别
+     * <p>
+     * /* 是所有文件夹，不含子文件夹
+     * <p>
+     * ** 的意思是所有文件夹及里面的子文件夹
+     */
+    @GetMapping("/**")
+    public ModelAndView all(HttpServletRequest request) {
+        return new ModelAndView(request.getServletPath());
+    }
+}
