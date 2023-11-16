01c2fb796ad3ab4eb729933d7e46d9900ab87049
zh_harry <zh_harry@163.com>
2023-11-16 22:51:50 +0800

    modify controller

--- src/main/java/com/sparrow/spring/starter/MvcConfigurerAdapter.java
+++ src/main/java/com/sparrow/spring/starter/MvcConfigurerAdapter.java
@@ -9,7 +9,6 @@ import com.sparrow.utility.StringUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
