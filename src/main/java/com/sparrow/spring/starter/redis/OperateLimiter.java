169e4202b04fa287dec3c0aa5843f62d4cd9ad94
张立志 <zhanglizhi@tedu.cn>
2023-11-25 13:17:02 +0800

    add starter

---
+++ src/main/java/com/sparrow/spring/starter/redis/OperateLimiter.java
@@ -0,0 +1,45 @@
+package com.sparrow.spring.starter.redis;
+
+import com.sparrow.exception.Asserts;
+import com.sparrow.protocol.BusinessException;
+import com.sparrow.protocol.constant.SparrowError;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
+
+public class OperateLimiter {
+
+    public OperateLimiter(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    private StringRedisTemplate redisTemplate;
+
+    public boolean renewalLimit(String key, Integer times, Long expires) {
+        Long currentTimes = redisTemplate.opsForValue().increment(key);
+        if (currentTimes == null) {
+            return false;
+        }
+        if (currentTimes > times) {
+            return false;
+        }
+        redisTemplate.expire(key, expires, TimeUnit.MILLISECONDS);
+        return true;
+    }
+
+    public boolean limit(String key, Integer times, Long expires) {
+        Long currentTimes = redisTemplate.opsForValue().increment(key);
+        //null when used in pipeline / transaction.
+        if (currentTimes == null) {
+            return false;
+        }
+        if (currentTimes > times) {
+            return false;
+        }
+        if (currentTimes == 1L) {
+            redisTemplate.expire(key, expires, TimeUnit.MILLISECONDS);
+        }
+        return true;
+    }
+}
