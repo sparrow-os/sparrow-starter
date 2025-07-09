17e80d868285a71d157b19ee4c71144e69b841b9
zh_harry <zh_harry@163.com>
2025-07-09 23:50:32 +0800

    modify spring context

--- src/main/java/com/sparrow/spring/starter/EnumsAutoConfiguration.java
+++ src/main/java/com/sparrow/spring/starter/EnumsAutoConfiguration.java
@@ -1,11 +1,17 @@
 package com.sparrow.spring.starter;
 
 import com.sparrow.spring.starter.config.SparrowConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
-@ConditionalOnProperty(prefix = "sparrow", name = "enums")
+@ConditionalOnProperty(prefix = "sparrow.enums", name = {"coder","business"})
+@Slf4j
 public class EnumsAutoConfiguration {
+    public EnumsAutoConfiguration() {
+        log.info("EnumsAutoConfiguration init");
+    }
+
     @Bean
     public EnumsContainer businessEnumsContainer(SparrowConfig sparrowConfig) throws ClassNotFoundException {
         return new EnumsContainer(sparrowConfig.getEnums().getBusiness());
