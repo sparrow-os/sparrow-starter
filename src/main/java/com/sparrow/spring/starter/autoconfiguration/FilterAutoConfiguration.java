8156eeb8e8cb61e065d7875e88d079b4f1734a29
张立志 <zhanglizhi@tedu.cn>
2025-08-22 21:22:30 +0800

    fixed: /无法正确跳转问题

--- src/main/java/com/sparrow/spring/starter/autoconfiguration/FilterAutoConfiguration.java
+++ src/main/java/com/sparrow/spring/starter/autoconfiguration/FilterAutoConfiguration.java
@@ -4,17 +4,14 @@ import com.sparrow.spring.starter.SpringServletContainer;
 import com.sparrow.spring.starter.config.SparrowConfig;
 import com.sparrow.spring.starter.filter.*;
 import com.sparrow.spring.starter.monitor.Monitor;
-import com.sparrow.spring.starter.resolver.ClientInfoArgumentResolvers;
 import com.sparrow.support.web.AbstractGlobalAttributeFilter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.servlet.Filter;
+import javax.inject.Inject;
 import java.util.List;
 
 @Slf4j
@@ -23,14 +20,15 @@ public class FilterAutoConfiguration {
         log.info("Sparrow Auto Configuration INIT");
     }
 
-    @Autowired
+    @Inject
     private SparrowConfig sparrowConfig;
 
-    @Autowired
+    @Inject
     private SpringServletContainer springServletContainer;
 
-    @Autowired
+    @Inject
     private Monitor monitor;
+
     @Bean
     public FlashFilter flashFilter() {
         return new FlashFilter();
@@ -69,18 +67,6 @@ public class FilterAutoConfiguration {
         return new SpringGlobalAttributeFilter(this.sparrowConfig);
     }
 
-
-
-    @Bean
-    public FilterRegistrationBean<Filter> flashFilterBean() {
-        FilterRegistrationBean<Filter> globalAttributeFilterBean = new FilterRegistrationBean<>();
-        globalAttributeFilterBean.setFilter(flashFilter());
-        globalAttributeFilterBean.addUrlPatterns("/*");
-        globalAttributeFilterBean.setName("flashFilter");
-        globalAttributeFilterBean.setOrder(1);
-        return globalAttributeFilterBean;
-    }
-
     // 不要轻意加该配置会将SimpleUrlHandlerMapping 提前，导致应用程序无法自定义跳转
 //    @Override
 //    public void addViewControllers(ViewControllerRegistry registry) {
@@ -88,26 +74,4 @@ public class FilterAutoConfiguration {
 //        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 //    }
 
-
-    @Bean
-    public FilterRegistrationBean<Filter> globalAttributeFilterBean() {
-        FilterRegistrationBean<Filter> globalAttributeFilterBean = new FilterRegistrationBean<>();
-        globalAttributeFilterBean.setFilter(globalAttributeFilter());
-        globalAttributeFilterBean.addUrlPatterns("/*");
-        globalAttributeFilterBean.setName("globalAttributeFilter");
-        globalAttributeFilterBean.setOrder(1);
-        //多个filter的时候order的数值越小 则优先级越高
-        return globalAttributeFilterBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean<Filter> clientInformationFilterBean() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(clientInformationFilter());
-        filterRegistrationBean.addUrlPatterns("/*");
-        filterRegistrationBean.setName("clientInformationFilter");
-        filterRegistrationBean.setOrder(0);
-        //多个filter的时候order的数值越小 则优先级越高
-        return filterRegistrationBean;
-    }
 }
