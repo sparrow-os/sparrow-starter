69e9a2473bad5b951624600cfafeda2e6fd2a98d
zh_harry <zh_harry@163.com>
2025-08-27 00:22:23 +0800

    modify spring context

--- src/main/java/com/sparrow/spring/starter/autoconfiguration/FilterRegistrationBeanAutoConfiguration.java
+++ src/main/java/com/sparrow/spring/starter/autoconfiguration/FilterRegistrationBeanAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.sparrow.spring.starter.autoconfiguration;
 
 import com.sparrow.spring.starter.filter.ClientInformationFilter;
+import com.sparrow.spring.starter.filter.SparrowCorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -10,16 +11,28 @@ import javax.servlet.Filter;
 public class FilterRegistrationBeanAutoConfiguration {
 
     private static final int CLIENT_INFORMATION_FILTER_ORDER = 0;
-    @Autowired
-    private ClientInformationFilter clientInformationFilter;
+    private static final int CORS_FILTER_ORDER = 1;
+
 
     @Bean
-    public FilterRegistrationBean<Filter> clientInformationFilterBean() {
+    public FilterRegistrationBean<Filter> clientInformationFilterBean(ClientInformationFilter clientInformationFilter) {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(clientInformationFilter);
         filterRegistrationBean.addUrlPatterns("/*");
         filterRegistrationBean.setName("clientInformationFilter");
         filterRegistrationBean.setOrder(CLIENT_INFORMATION_FILTER_ORDER);
+        //多个filter的时候order的数值越小 则优先级越高
+        return filterRegistrationBean;
+    }
+
+
+    @Bean
+    public FilterRegistrationBean<Filter> corsFilter(SparrowCorsFilter corsFilter) {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(corsFilter);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setName("corsFilter");
+        filterRegistrationBean.setOrder(CORS_FILTER_ORDER);
         //多个filter的时候order的数值越小 则优先级越高
         return filterRegistrationBean;
     }
